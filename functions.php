70a544d993fc0324306c0664371cf4fb746e5e36
Mitchell Tracy <mitchell@netgainseo.com>
2018-06-25 13:08:15 -0400

    Add Child Custom.js in Functions

--- functions.php
+++ functions.php
@@ -1,12 +1,20 @@
 <?php
 function my_theme_enqueue_styles() {
 
-    $parent_style = 'mc_main'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    $parent_style = 'mc_main';
+    $parent_custom = 'parent_custom';
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/main.css' );
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/main.css',
         array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+
+    wp_enqueue_script( $parent_custom, get_template_directory_uri() . '/js/custom.js' );
+    wp_enqueue_script( 'child-custom',
+        get_stylesheet_directory_uri() . '/js/custom.js',
+        array( $parent_custom ),
         wp_get_theme()->get('Version')
     );
 }
