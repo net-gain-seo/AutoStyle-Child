4de195edbe190f759a7c1a4f2adb849faa8a61ea
Mitchell Tracy <mitchell@netgainseo.com>
2018-06-25 16:43:17 -0400

    Nav Updates

--- header.php
+++ header.php
@@ -8,6 +8,12 @@
 </head>
 
 <body <?php body_class(); ?>>
+
+<div id="closeNav" class="close-nav">Close <i class="fa fa-times"></i></button></div>
+<nav id="mainNav" class="mobile-nav" role="navigation">
+   <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'container' => '' ) ); ?>
+</nav>
+
 <div id="bodyslide">
 	<header class="site-header">
 	    <div id="flexHeader" class="container flex-header padding0">
